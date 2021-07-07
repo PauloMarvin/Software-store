325a9c6e7d9cfd8b9aed4c2be7b3378caddd1ced
Marvin <paulo.marving@gmail.com>
2021-07-06 21:24:06 -0300

    Implantation of checkout

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from enum import unique
-
 from flask import Flask, render_template, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, HiddenField
+from wtforms import StringField, IntegerField, TextAreaField, HiddenField, SelectField
 from flask_wtf.file import FileField, FileAllowed
-import sqlite3
 
 app = Flask(__name__)
 
@@ -19,7 +16,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trendy.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'mysecret'
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
 configure_uploads(app, photos)
 
@@ -28,13 +24,6 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
-data_base = sqlite3.connect('trendy.db')
-
-
-class AddToCart(FlaskForm):
-    quantity = IntegerField('Quantity')
-    id = HiddenField('ID')
 
 
 class Product(db.Model):
@@ -46,6 +35,29 @@ class Product(db.Model):
     image = db.Column(db.String(100))
 
 
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reference = db.Column(db.String(5))
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    phone_number = db.Column(db.Integer)
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(20))
+    country = db.Column(db.String(20))
+    status = db.Column(db.String(10))
+    payment_type = db.Column(db.String(10))
+    items = db.relationship('Order_Item', backref='order', lazy=True)
+
+
+class Order_Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
+
+
 class AddProduct(FlaskForm):
     name = StringField('Name')
     price = IntegerField('Price')
@@ -54,32 +66,57 @@ class AddProduct(FlaskForm):
     image = FileField('Image', validators=[FileAllowed(IMAGES, 'Only images are accepted.')])
 
 
-class AddPublisher(FlaskForm):
-    name = StringField('Name')
-    price = IntegerField('Price')
-    stock = IntegerField('Stock')
-    description = TextAreaField('Description')
-    image = FileField('Image', validators=[FileAllowed(IMAGES, 'Only images are accepted.')])
+class AddToCart(FlaskForm):
+    quantity = IntegerField('Quantity')
+    id = HiddenField('ID')
 
 
-class AddDeveloper(FlaskForm):
-    name = StringField('Name')
-    price = IntegerField('Price')
-    stock = IntegerField('Stock')
-    description = TextAreaField('Description')
-    image = FileField('Image', validators=[FileAllowed(IMAGES, 'Only images are accepted.')])
+class Checkout(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    phone_number = StringField('Number')
+    email = StringField('Email')
+    address = StringField('Address')
+    city = StringField('City')
+    state = SelectField('State', choices=[('CA', 'California'), ('WA', 'Washington'), ('NV', 'Nevada')])
+    country = SelectField('Country', choices=[('US', 'United States'), ('UK', 'United Kingdom'), ('FRA', 'France')])
+    payment_type = SelectField('Payment Type', choices=[('CK', 'Check'), ('WT', 'Wire Transfer')])
+
+
+def handle_cart():
+    products = []
+    grand_total = 0
+    index = 0
+
+    for item in session['cart']:
+        product = Product.query.filter_by(id=item['id']).first()
+
+        quantity = int(item['quantity'])
+        total = quantity * product.price
+        grand_total += total
+
+        products.append({'id': product.id, 'name': product.name, 'price': product.price, 'image': product.image,
+                         'quantity': quantity, 'total': total, 'index': index})
+        index += 1
+
+    grand_total_plus_shipping = grand_total + 1000
+
+    return products, grand_total, grand_total_plus_shipping
 
 
 @app.route('/')
 def index():
     products = Product.query.all()
+
     return render_template('index.html', products=products)
 
 
 @app.route('/product/<id>')
 def product(id):
     product = Product.query.filter_by(id=id).first()
+
     form = AddToCart()
+
     return render_template('view-product.html', product=product, form=form)
 
 
@@ -94,7 +131,7 @@ def quick_add(id):
     return redirect(url_for('index'))
 
 
-@app.route('/add-to-cart', methods=['GET', 'POST'])
+@app.route('/add-to-cart', methods=['POST'])
 def add_to_cart():
     if 'cart' not in session:
         session['cart'] = []
@@ -110,29 +147,51 @@ def add_to_cart():
 
 @app.route('/cart')
 def cart():
-    products = []
+    products, grand_total, grand_total_plus_shipping = handle_cart()
 
-    for item in session['cart']:
-        product = Product.query.filter_by(id=item['id']).first()
-
-        quantity = int(item['quantity'])
-        total = quantity * product.price
-        products.append({'id': product.id, 'name': product.name, 'price': product.price,
-                         'image': product.image, 'quantity': quantity, 'total': total})
+    return render_template('cart.html', products=products, grand_total=grand_total,
+                           grand_total_plus_shipping=grand_total_plus_shipping)
 
 
-    return render_template('cart.html',products=products)
+@app.route('/remove-from-cart/<index>')
+def remove_from_cart(index):
+    del session['cart'][int(index)]
+    session.modified = True
+    return redirect(url_for('cart'))
 
 
-@app.route('/checkout')
+@app.route('/checkout', methods=['GET', 'POST'])
 def checkout():
-    return render_template('checkout.html')
+    form = Checkout()
+
+    if form.validate_on_submit():
+        products, grand_total, grand_total_plus_shipping = handle_cart()
+
+        order = Order()
+        form.populate_obj(order)
+        order.reference = 'ABCDE'
+        order.status = 'PENDING'
+
+        for product in products:
+            order_item = Order_Item(quantity=product['quantity'], product_id=product['id'])
+            order.items.append(order_item)
+
+        db.session.add(order)
+        db.session.commit()
+
+        session['cart'] = []
+        session.modified = True
+
+        return redirect(url_for('index'))
+
+    return render_template('checkout.html', form=form)
 
 
 @app.route('/admin')
 def admin():
     products = Product.query.all()
     products_in_stock = Product.query.filter(Product.stock > 0).count()
+
     return render_template('admin/index.html', admin=True, products=products, products_in_stock=products_in_stock)
 
 
@@ -160,4 +219,4 @@ def order():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
